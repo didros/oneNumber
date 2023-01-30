c8ff03084480047b34e83a44c6864a32ab79ffe0
Didier Rossi <did.rossi@gmail.com>
2023-01-30 21:36:16 +0100

    Update cheapEl.py

--- cheapEl.py
+++ cheapEl.py
@@ -12,8 +12,6 @@ now = datetime.now()
 offset = dt.timedelta(hours=float(r.text))
 new_time= now + offset
 
-st.text("At approximatly : " + new_time.strftime("%H:%M"))
-
 st.title("Når er strøm billiste i dag?")
 
-st.metric("kl. " + new_time.strftime("%H:%M") + ", om", "om " + r.text + " timer")
+st.metric("kl. " + new_time.strftime("%H:%M") + ", dvs. om", r.text + " timer")
