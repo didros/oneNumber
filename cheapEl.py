acb49e798154bd17ebd2da03e832569f754b43b7
Didier Rossi <didierrossi@Air-de-Didier.lan>
2023-01-31 21:49:14 +0100

    arrondis les valeurs

--- cheapEl.py
+++ cheapEl.py
@@ -14,12 +14,25 @@ now = datetime.now(pytz.timezone('Europe/Oslo'))
 offset = dt.timedelta(hours=float(r.text))
 #offset = dt.timedelta(hours=float("3"))
 new_time= now + offset
-
+    
 if h == 0 :
-    t = "Nu ! altså om :"
+    t = "Nu !"
 else :
-    t = "kl. " + new_time.strftime("%H:%M") + "m dvs. om"
+    t = "kl. " + new_time.strftime("%H") + ", altså om ca."
+
+if ( new_time.minute >= 30 ) :
+    h -= 0.5
 
 st.title("Når er strøm billigste i dag før kl. 8.00 i Oslo?")
+st.metric(t, str(h) + " timer")
 
-st.metric(t, r.text + " timer")
+
+timewindow = st.radio(
+    "Time window currently selected:",
+    ('3 hours', '2 hours', '1 hour'),
+    horizontal=True)
+
+if timewindow == 'Comedy':
+    st.write('You selected comedy.')
+else:
+    st.write("You didn\'t select comedy.")
