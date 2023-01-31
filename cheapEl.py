833cf1be196b5b9af97b36324d3c5a5c617f713c
Didier Rossi <didierrossi@Air-de-Didier.lan>
2023-01-31 20:29:57 +0100

    .

--- cheapEl.py
+++ cheapEl.py
@@ -5,14 +5,21 @@ import pytz
 import requests
 
 URL = "https://" + st.secrets.jeedom_host + "/core/api/jeeApi.php"
-PARAMS = {'apikey':st.secrets.jeedom_api_key, 'type':'cmd', 'id':'2241'}
+PARAMS = {'apikey':st.secrets.jeedom_api_key, 'type':'cmd', 'id':'2046'}
 
 r = requests.get(url = URL, params = PARAMS)
+h = float(r.text)
 
 now = datetime.now(pytz.timezone('Europe/Oslo'))
-offset = dt.timedelta(minutes=int(r.text))
+offset = dt.timedelta(hours=float(r.text))
+#offset = dt.timedelta(hours=float("3"))
 new_time= now + offset
 
-st.title("Når er strøm billigste i dag?")
+if h == 0 :
+    t = "Nu ! altså om :"
+else :
+    t = "kl. " + new_time.strftime("%H:%M") + "m dvs. om"
 
-st.metric("kl. " + new_time.strftime("%H:%M") + ", dvs. om", r.text + " timer")
+st.title("Når er strøm billigste i dag før kl. 8.00 i Oslo?")
+
+st.metric(t, r.text + " timer")
