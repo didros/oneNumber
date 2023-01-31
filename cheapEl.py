40b39a20b7311bba9dc4b1ede74501601600462f
Didier Rossi <didierrossi@Air-de-Didier.lan>
2023-01-31 22:15:39 +0100

    with radio button almost operational

--- cheapEl.py
+++ cheapEl.py
@@ -4,11 +4,15 @@ import datetime as dt
 import pytz
 import requests
 
+if "jeedom_cmd" not in st.session_state:
+    st.session_state.jeedom_cmd = '2046'
+
 URL = "https://" + st.secrets.jeedom_host + "/core/api/jeeApi.php"
-PARAMS = {'apikey':st.secrets.jeedom_api_key, 'type':'cmd', 'id':'2046'}
+PARAMS = {'apikey':st.secrets.jeedom_api_key, 'type':'cmd', 'id':st.session_state.jeedom_cmd}
 
 r = requests.get(url = URL, params = PARAMS)
 h = float(r.text)
+"debug : " + r.text
 
 now = datetime.now(pytz.timezone('Europe/Oslo'))
 offset = dt.timedelta(hours=float(r.text))
@@ -27,12 +31,22 @@ st.title("Når er strøm billigste i dag før kl. 8.00 i Oslo?")
 st.metric(t, str(h) + " timer")
 
 
+selVal = ['1 hour','2 hours','3 hours','4 hours']
 timewindow = st.radio(
     "Time window currently selected:",
-    ('3 hours', '2 hours', '1 hour'),
+    selVal,
+    index=2,
     horizontal=True)
 
-if timewindow == 'Comedy':
-    st.write('You selected comedy.')
-else:
-    st.write("You didn\'t select comedy.")
+
+if timewindow == '1 hour' :
+    st.session_state.jeedom_cmd = "2048" 
+if timewindow == '2 hours' :
+    st.session_state.jeedom_cmd = "2049" 
+if timewindow == '3 hours' :
+    st.session_state.jeedom_cmd = "2046" 
+if timewindow == '4 hours' :
+    st.session_state.jeedom_cmd = "2047" 
+
+timewindow
+st.session_state.jeedom_cmd
