3195e0e8c9574d8c3937b977f02dad3fcc7c65c7
Didier Rossi <didierrossi@Air-de-Didier.lan>
2023-02-01 07:44:36 +0100

    more structure

--- cheapEl.py
+++ cheapEl.py
@@ -4,49 +4,61 @@ import datetime as dt
 import pytz
 import requests
 
-if "jeedom_cmd" not in st.session_state:
-    st.session_state.jeedom_cmd = '2046'
+def fetch(url):
+    try:
+        r = requests.get(url = URL, params = PARAMS)
+        
+    except Exception:
+        return {}
 
-URL = "https://" + st.secrets.jeedom_host + "/core/api/jeeApi.php"
-PARAMS = {'apikey':st.secrets.jeedom_api_key, 'type':'cmd', 'id':st.session_state.jeedom_cmd}
+def main():
 
-r = requests.get(url = URL, params = PARAMS)
-h = float(r.text)
-"debug : " + r.text
+    if "jeedom_cmd" not in st.session_state:
+        st.session_state.jeedom_cmd = '2046'
 
-now = datetime.now(pytz.timezone('Europe/Oslo'))
-offset = dt.timedelta(hours=float(r.text))
-#offset = dt.timedelta(hours=float("3"))
-new_time= now + offset
-    
-if h == 0 :
-    t = "Nu !"
-else :
-    t = "kl. " + new_time.strftime("%H") + ", altså om ca."
+    URL = "https://" + st.secrets.jeedom_host + "/core/api/jeeApi.php"
+    PARAMS = {'apikey':st.secrets.jeedom_api_key, 'type':'cmd', 'id':st.session_state.jeedom_cmd}
 
-if ( new_time.minute >= 30 ) :
-    h -= 0.5
+    r = requests.get(url = URL, params = PARAMS)
+    h = float(r.text)
+    "debug : " + r.text
 
-st.title("Når er strøm billigste i dag før kl. 8.00 i Oslo?")
-st.metric(t, str(h) + " timer")
+    now = datetime.now(pytz.timezone('Europe/Oslo'))
+    offset = dt.timedelta(hours=float(r.text))
+    #offset = dt.timedelta(hours=float("3"))
+    new_time= now + offset
+        
+    if h == 0 :
+        t = "Nu !"
+    else :
+        t = "kl. " + new_time.strftime("%H") + ", altså om ca."
 
+    if ( new_time.minute >= 30 ) :
+        h -= 0.5
 
-selVal = ['1 hour','2 hours','3 hours','4 hours']
-timewindow = st.radio(
-    "Time window currently selected:",
-    selVal,
-    index=2,
-    horizontal=True)
+    st.title("Når er strøm billigste i dag før kl. 8.00 i Oslo?")
+    st.metric(t, str(h) + " timer")
 
 
-if timewindow == '1 hour' :
-    st.session_state.jeedom_cmd = "2048" 
-if timewindow == '2 hours' :
-    st.session_state.jeedom_cmd = "2049" 
-if timewindow == '3 hours' :
-    st.session_state.jeedom_cmd = "2046" 
-if timewindow == '4 hours' :
-    st.session_state.jeedom_cmd = "2047" 
+    selVal = ['1 hour','2 hours','3 hours','4 hours']
+    timewindow = st.radio(
+        "Time window currently selected:",
+        selVal,
+        index=2,
+        horizontal=True)
 
-timewindow
-st.session_state.jeedom_cmd
+
+    if timewindow == '1 hour' :
+        st.session_state.jeedom_cmd = "2048" 
+    if timewindow == '2 hours' :
+        st.session_state.jeedom_cmd = "2049" 
+    if timewindow == '3 hours' :
+        st.session_state.jeedom_cmd = "2046" 
+    if timewindow == '4 hours' :
+        st.session_state.jeedom_cmd = "2047" 
+
+    timewindow
+    st.session_state.jeedom_cmd
+
+if __name__ == '__main__' :
+    main()
