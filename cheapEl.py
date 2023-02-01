ad424f28529cdb9049bad0eb0556ddd79ea21ce2
Didier Rossi <didierrossi@Air-de-Didier.lan>
2023-02-01 22:42:50 +0100

    Version 1.0 TODO : - Handling of exception - caching? - auto refresh (like at 30 and time shift)

--- cheapEl.py
+++ cheapEl.py
@@ -4,61 +4,67 @@ import datetime as dt
 import pytz
 import requests
 
-def fetch(url):
+def fetch():
+
     try:
-        r = requests.get(url = URL, params = PARAMS)
-        
+        if "input_timew" not in st.session_state:
+            timewindow = '3 hours'
+        else:
+            timewindow = st.session_state.input_timew
+
+        if timewindow == '1 hour' :
+            st.session_state.jeedom_cmd = "2048" 
+        if timewindow == '2 hours' :
+            st.session_state.jeedom_cmd = "2049" 
+        if timewindow == '3 hours' :
+            st.session_state.jeedom_cmd = "2046" 
+        if timewindow == '4 hours' :
+            st.session_state.jeedom_cmd = "2047"
+
+        PARAMS = {'apikey':st.secrets.jeedom_api_key, 'type':'cmd', 'id':st.session_state.jeedom_cmd}
+        URL = "https://" + st.secrets.jeedom_host + "/core/api/jeeApi.php"
+        st.session_state.result = requests.get(url = URL, params = PARAMS).text
+
     except Exception:
-        return {}
+        "Oups .... backend down!"
 
-def main():
+def show():
 
-    if "jeedom_cmd" not in st.session_state:
-        st.session_state.jeedom_cmd = '2046'
-
-    URL = "https://" + st.secrets.jeedom_host + "/core/api/jeeApi.php"
-    PARAMS = {'apikey':st.secrets.jeedom_api_key, 'type':'cmd', 'id':st.session_state.jeedom_cmd}
-
-    r = requests.get(url = URL, params = PARAMS)
-    h = float(r.text)
-    "debug : " + r.text
+    h = float(st.session_state.result)
 
     now = datetime.now(pytz.timezone('Europe/Oslo'))
-    offset = dt.timedelta(hours=float(r.text))
-    #offset = dt.timedelta(hours=float("3"))
+    offset = dt.timedelta(hours=h)
     new_time= now + offset
         
-    if h == 0 :
+    if h == 0:
         t = "Nu !"
-    else :
-        t = "kl. " + new_time.strftime("%H") + ", altså om ca."
+    else:
+        t = "kl. " + new_time.strftime("%-H.00") + ", dvs. om ca."
 
     if ( new_time.minute >= 30 ) :
         h -= 0.5
 
-    st.title("Når er strøm billigste i dag før kl. 8.00 i Oslo?")
     st.metric(t, str(h) + " timer")
 
 
+def main():
+
+    st.title("Når er strøm billigste i dag (i Oslo)?")
+
+    if "input_timew" not in st.session_state:
+        fetch()
+
+    show()
+
     selVal = ['1 hour','2 hours','3 hours','4 hours']
     timewindow = st.radio(
-        "Time window currently selected:",
+        "Tidsvindu valgt:",
         selVal,
         index=2,
-        horizontal=True)
+        horizontal=True,
+        key="input_timew",
+        on_change=fetch)
 
-
-    if timewindow == '1 hour' :
-        st.session_state.jeedom_cmd = "2048" 
-    if timewindow == '2 hours' :
-        st.session_state.jeedom_cmd = "2049" 
-    if timewindow == '3 hours' :
-        st.session_state.jeedom_cmd = "2046" 
-    if timewindow == '4 hours' :
-        st.session_state.jeedom_cmd = "2047" 
-
-    timewindow
-    st.session_state.jeedom_cmd
 
 if __name__ == '__main__' :
     main()
