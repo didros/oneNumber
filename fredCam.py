a73d76d794eb7aeb553f1518a983002f0eb0fb05
Didier Rossi <didierrossi@Air-de-Didier.lan>
2023-02-11 10:54:51 +0100

    .

--- fredCam.py
+++ fredCam.py
@@ -1,20 +1,51 @@
 import streamlit as st
+import json
 
 from streamlit_echarts import st_echarts
 from datetime import datetime
 import datetime as dt
 
+from streamlit_javascript import st_javascript
+
 global budget
 global depense
+global fcCon
+js    = """localStorage.fcConfig || "{}" """
+retJs = st_javascript(js)
+
+if type(retJs).__name__ == "str":
+    fcCon = json.loads(retJs)
+#fcCon
+
+with st.expander("Personlig info"):
+
+    if "salary" not in fcCon:
+        fcCon["salary"] = 50000
+    fcCon["salary"] = st.number_input('Angi ditt lønn siste måned', step=100, value = int(fcCon["salary"]))
+
+    if "perhour" not in fcCon:
+        fcCon["perhour"] = 100
+    else:
+        if fcCon["perhour"] == 0 :
+            fcCon["perhour"] = 100
+    fcCon["perhour"] = st.number_input('Lønn per time', step=10, value = int(fcCon["perhour"]), min_value=100)
+
+    HoursPerWeek = 37.5
+    HoursPerDay  = HoursPerWeek / 5
+    salaryPerMonth = fcCon["salary"]
+    perHour= salaryPerMonth / (HoursPerWeek*4)
+    hours = salaryPerMonth / fcCon["perhour"] 
+    "Du har jobbet ca. " + str(int(hours)) + " timer forrige måned."
+
+    js    = """localStorage.setItem( "fcConfig", "{\\"salary\\":"""
+    js   += str(fcCon["salary"]) 
+    js   += """, """
+    js   += """\\"perhour\\":"""
+    js   += str(fcCon["perhour"]) 
+    js   += """}" )"""
+    retJs = st_javascript(js)
 
 
-HoursPerWeek = 37.5
-HoursPerDay  = HoursPerWeek / 5
-salaryPerMonth = 20000
-
-perHour= salaryPerMonth / (HoursPerWeek*4)
-
-"Du tjener ca. " + str(int(perHour)) + " kr per time."
 
 depense = st.number_input('Angi pris (kr)', step=10)
 
